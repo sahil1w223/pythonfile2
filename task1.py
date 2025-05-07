d6fbbce36d7fef0c482466e6a7afbe4b0b211986
sahil1w223 <sahiljal751@gmail.com>
2025-05-07 15:49:44 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,5 @@
+user_number = int(input("enter a number : "))
+if user_number % 2 == 0:
+    print("even number")
+else:   
+    print("odd number")
---
+++ task2.py
@@ -0,0 +1,4 @@
+num = 0
+for i in range (1,50):
+    num = num + i
+print("sum of first 50 numbers is : ", num)
